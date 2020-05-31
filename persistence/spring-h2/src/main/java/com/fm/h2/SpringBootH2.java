08ce4a3984a31ff5e9c524ae5f941f89994e0e99
cosmin_bucur <cosmindanbucur@gmail.com>
2020-05-31 20:16:37 +0300

    add spring boot project

---
+++ persistence/spring-h2/src/main/java/com/fm/h2/SpringBootH2.java
@@ -0,0 +1,14 @@
+package com.fm.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootH2.class);
+    }
+}
+
+
